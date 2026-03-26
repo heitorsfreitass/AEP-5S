48e95c7c06fd9cda6478210ff3ddd9420724c005
Heitor Freitas <freitasheitor102@gmail.com>
2026-03-26 08:36:22 -0300

    implement repositories

--- src/Repositories/SolicitacaoRepository.java
+++ src/Repositories/SolicitacaoRepository.java
@@ -1,4 +1,53 @@
 package Repositories;
 
-public interface SolicitacaoRepository {
+import Enums.Categoria;
+import Enums.StatusSolicitacao;
+import Models.Solicitacao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SolicitacaoRepository {
+    private List<Solicitacao> solicitacoes = new ArrayList<>();
+
+    public void salvar(Solicitacao solicitacao)
+    {
+        solicitacoes.add(solicitacao);
+    }
+
+    public Solicitacao buscarPorProtocolo(String protocolo) {
+        Solicitacao resultado = null;
+        for (int i = 0; i < solicitacoes.size(); i++) {
+            if (solicitacoes.get(i).getProtocolo().equalsIgnoreCase(protocolo)) {
+                resultado = solicitacoes.get(i);
+            }
+        }
+        return resultado;
+    }
+
+    public List<Solicitacao> listarTodas()
+    {
+        return solicitacoes;
+    }
+
+    public List<Solicitacao> filtrarPorStatusECategoria(StatusSolicitacao status, Categoria categoria) {
+        List<Solicitacao> resultado = new ArrayList<>();
+        for (Solicitacao s : solicitacoes) {
+            if (s.getStatus() == status && s.getCategoria() == categoria) {
+                resultado.add(s);
+            }
+        }
+        return resultado;
+    }
+
+    //-- Dar um jeito de não usar loop for com if
+    //-- Separar responsabilidades do filtrarPorStatusECategoria()
+
+    public List<Solicitacao> filtrarPorLocalizacao(String localizacao)
+    {
+        return solicitacoes.stream()
+                .filter(s -> s.getLocalizacao().equalsIgnoreCase(localizacao))
+                .collect(Collectors.toList());
+    }
 }
--- src/Repositories/UsuarioRepository.java
+++ src/Repositories/UsuarioRepository.java
@@ -1,4 +1,32 @@
 package Repositories;
 
-public interface UsuarioRepository {
+import Models.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioRepository {
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    public void salvar(Usuario usuario)
+    {
+        usuarios.add(usuario);
+    }
+
+    public Usuario buscarPorId(String id)
+    {
+        Usuario resultado = null;
+
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i).getId().equalsIgnoreCase(id)) {
+                resultado = usuarios.get(i);
+            }
+        }
+        return resultado;
+    }
+
+    public List<Usuario> listarTodos()
+    {
+        return usuarios;
+    }
 }
