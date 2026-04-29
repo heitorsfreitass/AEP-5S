18f310280fd733d6082b08d9b9ba81953143d579
Heitor Freitas <freitasheitor102@gmail.com>
2026-04-29 09:26:05 -0300

    create web spring app structure

---
+++ web/src/main/java/com/example/aep_web/AepWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.aep_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AepWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AepWebApplication.class, args);
+	}
+
+}
